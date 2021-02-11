dd7e0f9419b018348db13953456fbb7e2dd6f2c9
Lydia <Lydia.Fritz@gcu.edu>
2021-02-11 08:27:33 -0700

    Add files via upload
    files for in-class exercise

---
+++ Card.h
@@ -0,0 +1,27 @@
+#ifndef CARD_H
+#define CARD_H
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Card {
+private:
+	int value;
+	string suit;
+public:
+	Card(int, string);
+	Card();
+	int getValue()const;
+	string getSuit()const;
+
+	//overload the > operator
+	bool operator>(const Card& rhs);
+	bool operator==(const Card& rhs);
+	friend ostream& operator<<(ostream& out, const Card& c);
+};
+
+
+#endif // !CARD_H
+
